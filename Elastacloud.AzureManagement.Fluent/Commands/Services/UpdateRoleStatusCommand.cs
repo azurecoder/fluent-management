b3718bd217f6c15dec2697a7ca0ec0c64bae405d
azurecoder <richard@elastacloud.com>
2012-10-24 11:41:56 +0100

    Updated the virtual machine manager to change construction accessibility to internal Added commands and RoleContextManager to allow a role/deployment context with two methods start/stop
    Known Issues
    ------------
    Not trapping an Http Status 409 conflict if the fabric is in mid-operation
    
    Signed-off-by: azurecoder <richard@elastacloud.com>

---
+++ Elastacloud.AzureManagement.Fluent/Commands/Services/UpdateRoleStatusCommand.cs
@@ -0,0 +1,56 @@
+﻿/************************************************************************************************************
+ * This software is distributed under a GNU Lesser License by Elastacloud Limited and it is free to         *
+ * modify and distribute providing the terms of the license are followed. From the root of the source the   *
+ * license can be found in /Resources/license.txt                                                           * 
+ *                                                                                                          *
+ * Web at: www.elastacloud.com                                                                              *
+ * Email: info@elastacloud.com                                                                              *
+ ************************************************************************************************************/
+
+using System;
+using System.Text;
+using System.Xml.Linq;
+using Elastacloud.AzureManagement.Fluent.Helpers;
+using Elastacloud.AzureManagement.Fluent.Types;
+
+namespace Elastacloud.AzureManagement.Fluent.Commands.Services
+{
+    /// <summary>
+    /// Used for updating the status of a role between running and suspended
+    /// </summary>
+    internal class UpdateRoleStatusCommand : ServiceCommand
+    {
+        /// <summary>
+        /// Constructs a command to create an update to the status of a role
+        /// </summary>
+        internal UpdateRoleStatusCommand(string serviceName, DeploymentSlot slot, UpdateDeploymentStatus status)
+        {
+            OperationId = "hostedservices";
+            ServiceType = "services";
+            HttpCommand = serviceName + "/deploymentslots/" + slot.ToString().ToLower() + "/?comp=status";
+            Status = status;
+        }
+
+        protected UpdateDeploymentStatus Status { get; set; }
+
+        /// <summary>
+        /// The deployment slot being used production|staging
+        /// </summary>
+        internal DeploymentSlot DeploymentSlot { get; set; }
+
+        
+        //https://management.core.windows.net/<subscription-id>/services/hostedservices/<service-name>/deploymentslots/<deployment-slot-name>/?comp=status
+        /// <summary>
+        /// The Xml payload that is created and sent to the Fabric with the create deployment parameters
+        /// </summary>
+        /// <returns>A string Xml document representation</returns>
+        protected override string CreateXmlPayload()
+        {
+            XNamespace ns = "http://schemas.microsoft.com/windowsazure";
+            var doc = new XDocument(
+                new XDeclaration("1.0", "utf-8", ""),
+                new XElement(ns + "UpdateDeploymentStatus", new XElement(ns + "Status", Status)));
+            return doc.ToStringFullXmlDeclaration();
+        }
+    }
+}
---
+++ Elastacloud.AzureManagement.Fluent/Fluent API/Roles/IRoleOperation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Elastacloud.AzureManagement.Fluent.Roles
+{
+    /// <summary>
+    /// Used to define the operations that can take place on the role
+    /// </summary>
+    public interface IRoleOperation : IDisposable
+    {
+        /// <summary>
+        /// Used to start a role
+        /// </summary>
+        void Start();
+        /// <summary>
+        /// Used to stop a role
+        /// </summary>
+        void Stop();
+    }
+}
