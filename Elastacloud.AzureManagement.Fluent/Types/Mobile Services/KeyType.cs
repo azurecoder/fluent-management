a259153483edeff646f22593194dcfffa4921c0e
Richard Conway <richard@elastacloud.com>
2012-12-27 22:56:13 +0000

    Added keytype to allow keys to be regenerated for mobile service
    Signed-off-by: Richard Conway <richard@elastacloud.com>

---
+++ Elastacloud.AzureManagement.Fluent/Types/Mobile Services/KeyType.cs
@@ -0,0 +1,25 @@
+﻿/************************************************************************************************************
+ * This software is distributed under a GNU Lesser License by Elastacloud Limited and it is free to         *
+ * modify and distribute providing the terms of the license are followed. From the root of the source the   *
+ * license can be found in /Resources/license.txt                                                           * 
+ *                                                                                                          *
+ * Web at: www.elastacloud.com                                                                              *
+ * Email: info@elastacloud.com                                                                              *
+ ************************************************************************************************************/
+namespace Elastacloud.AzureManagement.Fluent.Types.MobileServices
+{
+    /// <summary>
+    /// the type of key used by mobile services
+    /// </summary>
+    public enum KeyType
+    {
+        /// <summary>
+        /// The application key used by the mobile service
+        /// </summary>
+        Application,
+        /// <summary>
+        /// The master key used by the mobile service
+        /// </summary>
+        Master
+    }
+}
