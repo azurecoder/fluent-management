9422dfb10c71e5701e2a809208e85533a5e37c90
Richard Conway <richard@elastacloud.com>
2013-02-20 17:15:20 +0000

    updated client to use an applicationfactory - have to refactor to use an abstract factory pattern
    Signed-off-by: Richard Conway <richard@elastacloud.com>

---
+++ Elastacloud.AzureManagement.Fluent.Console/ApplicationFactory.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Elastacloud.AzureManagement.Fluent.Helpers.PublishSettings;
+
+namespace Elastacloud.AzureManagement.Fluent.Console
+{
+    public class ApplicationFactory
+    {
+        public ApplicationFactory(string term, string operation, string subscriptionId, string publishSettings,
+                                  string cloudServiceName, string deploymentName, string roleName, string password)
+        {
+            Term = term;
+            Operation = operation;
+            SubscriptionId = subscriptionId;
+            CloudServiceName = cloudServiceName;
+            RoleName = roleName;
+            Password = password;
+            DeploymentName = deploymentName;
+            ManagementCertificate = GetCertificateFromPublishSettings(publishSettings);
+            PublishSettingsRoot = Path.GetDirectoryName(publishSettings);
+        }
+
+        internal string DeploymentName { get; private set; }
+        internal X509Certificate2 ManagementCertificate { get; private set; }
+        internal string Password { get; private set; }
+        internal string RoleName { get; private set; }
+        internal string CloudServiceName { get; private set; }
+        internal string SubscriptionId { get; private set; }
+        internal string Operation { get; private set; }
+        internal string Term { get; private set; }
+        internal string PublishSettingsRoot { get; private set; }
+
+        public IExecute GetExecutor()
+        {
+            if (Term == "mobile" && Operation == "create")
+            {
+                return new MobileCreate(this);
+            }
+            if (Term == "mobile" && Operation == "read")
+            {
+                return new MobileGet(this);
+            }
+            if (Term == "vm" && Operation == "create")
+            {
+                return new VmCreate(this);
+            }
+            if (Term == "vm" && Operation == "read")
+            {
+                return new VmGet(this);
+            }
+            return null;
+        }
+
+        private X509Certificate2 GetCertificateFromPublishSettings(string publishSettings)
+        {
+            var settings = new PublishSettingsExtractor(publishSettings);
+            return settings.AddPublishSettingsToPersonalMachineStore();
+        }
+    }
+}
---
+++ Elastacloud.AzureManagement.Fluent.Console/VmGet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Elastacloud.AzureManagement.Fluent.Clients;
+using Elastacloud.AzureManagement.Fluent.Commands.VirtualMachines;
+using Elastacloud.AzureManagement.Fluent.Helpers;
+using Elastacloud.AzureManagement.Fluent.Types;
+using Elastacloud.AzureManagement.Fluent.Types.VirtualMachines;
+using Elastacloud.AzureManagement.Fluent.VirtualMachines.Classes;
+
+namespace Elastacloud.AzureManagement.Fluent.Console
+{
+    public class VmGet : IExecute
+    {
+        private readonly ApplicationFactory _applicationFactory;
+
+        public VmGet(ApplicationFactory factory)
+        {
+            _applicationFactory = factory;
+        }
+
+        #region Implementation of IExecute
+
+        public void Execute()
+        {
+            var properties = new WindowsVirtualMachineProperties()
+            {
+                AdministratorPassword = _applicationFactory.Password,
+                RoleName = _applicationFactory.RoleName,
+                Certificate = _applicationFactory.ManagementCertificate,
+                SubscriptionId = _applicationFactory.SubscriptionId,
+                CloudServiceName = _applicationFactory.CloudServiceName,
+                DeploymentName = _applicationFactory.DeploymentName
+            };
+            var client = new WindowsVirtualMachineClient(properties);
+            var vm = client.VirtualMachine;
+
+            System.Console.WriteLine("Virtual machine client");
+            System.Console.WriteLine("======================");
+            System.Console.WriteLine("Role name: {0}", vm.RoleName);
+            System.Console.WriteLine("Role size: {0}", vm.RoleSize.ToString());
+
+            string rdpFile = Path.Combine(_applicationFactory.PublishSettingsRoot, "vm.rdp");
+            client.SaveRemoteDesktopFile(rdpFile);
+            System.Console.WriteLine("RDP file saved to: {0}", rdpFile);
+        }
+
+        public void ParseTokens(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
