7ce15550367e3380ee5e6f757b3b32945707b65a
Richard Conway <richard@elastacloud.com>
2013-02-27 21:57:21 +0000

    added url test helper

---
+++ Elastacloud.AzureManagement.Fluent.Tests/TestHttpUrlParse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using Elastacloud.AzureManagement.Fluent.Helpers;
+using NUnit.Framework;
+
+namespace Elastacloud.AzureManagement.Fluent.Tests
+{
+    [TestFixture]
+    public class TestHttpUrlParse
+    {
+        [Test]
+        public void TestUrl()
+        {
+            const string url = "http://elastastorage.blob.core.windows.net/vm-images/imkzmoeb.yfo20130120221739.vhd";
+
+            var helper = new UrlHelper(url);
+
+            Assert.AreEqual("elastastorage.blob.core.windows.net", helper.HostFullDomain);
+            Assert.AreEqual("vm-images", helper.Path);
+            Assert.AreEqual("imkzmoeb.yfo20130120221739.vhd", helper.File);
+            Assert.AreEqual("elastastorage", helper.HostSubDomain);
+        }
+    }
+}
