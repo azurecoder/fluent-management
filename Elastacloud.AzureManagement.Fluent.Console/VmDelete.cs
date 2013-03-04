a09d74c6dedb0c4ed3a72f4aeb827d72dfa24fcb
Richard Conway <richard@elastacloud.com>
2013-03-04 20:37:26 +0000

    added the delete to enable the delete of virtual machines from the console app

---
+++ Elastacloud.AzureManagement.Fluent.Console/VmDelete.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Elastacloud.AzureManagement.Fluent.Clients;
+using Elastacloud.AzureManagement.Fluent.VirtualMachines.Classes;
+
+namespace Elastacloud.AzureManagement.Fluent.Console
+{
+    public class VmDelete : IExecute
+    {
+        private readonly ApplicationFactory _applicationFactory;
+
+        public VmDelete(ApplicationFactory factory)
+        {
+            _applicationFactory = factory;
+        }
+
+        #region Implementation of IExecute
+
+        public void Execute()
+        {
+            var properties = new WindowsVirtualMachineProperties()
+            {
+                AdministratorPassword = _applicationFactory.Password,
+                RoleName = _applicationFactory.RoleName,
+                Certificate = _applicationFactory.ManagementCertificate,
+                SubscriptionId = _applicationFactory.SubscriptionId,
+                CloudServiceName = _applicationFactory.CloudServiceName,
+                DeploymentName = _applicationFactory.DeploymentName
+            };
+            var client = new WindowsVirtualMachineClient(properties);
+            var vm = client.VirtualMachine;
+            client.DeleteVirtualMachine(true, true);
+            
+            System.Console.WriteLine("Deleted virtual machine + disks");
+        }
+
+        public void ParseTokens(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
