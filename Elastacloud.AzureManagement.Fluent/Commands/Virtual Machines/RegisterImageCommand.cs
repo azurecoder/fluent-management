44b6b5e110c7f2614d7b858f8684731e2c8839ea
Richard Conway <richard@elastacloud.com>
2014-02-02 14:59:11 +0000

    added new image properties and register image command to facilitate the intra subscription copy process

---
+++ Elastacloud.AzureManagement.Fluent/Commands/Virtual Machines/RegisterImageCommand.cs
@@ -0,0 +1,91 @@
+﻿/************************************************************************************************************
+ * This software is distributed under a GNU Lesser License by Elastacloud Limited and it is free to         *
+ * modify and distribute providing the terms of the license are followed. From the root of the source the   *
+ * license can be found in /Resources/license.txt                                                           * 
+ *                                                                                                          *
+ * Web at: www.elastacloud.com                                                                              *
+ * Email: info@elastacloud.com                                                                              *
+ ************************************************************************************************************/
+
+using System;
+using System.Globalization;
+using System.Xml.Linq;
+using Elastacloud.AzureManagement.Fluent.Commands.Services;
+using Elastacloud.AzureManagement.Fluent.Helpers;
+using Elastacloud.AzureManagement.Fluent.Types.VirtualMachines;
+
+namespace Elastacloud.AzureManagement.Fluent.Commands.VirtualMachines
+{
+    /// <summary>
+    ///   Registers a virtual machine image for either Linux or Windowss     
+    ///  </summary>
+    public class RegisterImageCommand : ServiceCommand
+    {
+        // https://management.core.windows.net/<subscription-id>/services/images        
+        /// <summary>
+        ///   Registers a virtual machine image for either Linux or Windowss     
+        ///  </summary>
+        internal RegisterImageCommand(ImageProperties properties)
+        {
+            AdditionalHeaders["x-ms-version"] = "2012-08-01";
+            OperationId = "images";
+            ServiceType = "services";
+            HttpVerb = HttpVerbPost;
+            properties.PublishedDate = DateTime.UtcNow;
+            properties.IsPremium = properties.IsPremium.HasValue && properties.IsPremium.Value;
+            properties.ShowInGui = properties.ShowInGui.HasValue && properties.ShowInGui.Value; 
+            Properties = properties;
+        }
+
+        /// <summary>
+        /// The full virtual machine properties of the windows instance the needs to be deployed
+        /// </summary>
+        public ImageProperties Properties { get; set; }
+
+        /// <summary>
+        /// Creates a deployment payload for a predefined template 
+        /// </summary>
+        /// <returns>A string xml representation</returns>
+        /// POST https://management.core.windows.net/<subscription-id>/services/hostedservices/<service-name>/deployments/<deployment-name>/roleinstances/<role-name>/Operations
+        protected override string CreatePayload()
+        {
+            /*<OSImage xmlns="http://schemas.microsoft.com/windowsazure" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">
+               <Label>image-label</Label>
+               <MediaLink>uri-of-the-containing-blob</MediaLink>
+               <Name>image-name</Name>
+               <OS>Linux|Windows</OS>
+               <Eula>image-eula</Eula>
+               <Description>image-description</Description>
+               <ImageFamily>image-family</ImageFamily>
+               <PublishedDate>published-date</PublishedDate>
+               <IsPremium>true/false</IsPremium>
+               <ShowInGui>true/false</ShowInGui>
+               <PrivacyUri>http://www.example.com/privacypolicy.html</PrivacyUri>
+               <IconUri>http://www.example.com/favicon.png</IconUri>
+               <RecommendedVMSize>Small/Large/Medium/ExtraLarge</RecommendedVMSize>
+               <SmallIconUri>http://www.example.com/smallfavicon.png</SmallIconUri>
+               <Language>language-of-image</Language>
+            </OSImage>*/
+            XNamespace ns = "http://schemas.microsoft.com/windowsazure";
+            var doc = new XDocument(
+                new XDeclaration("1.0", "utf-8", ""),
+                new XElement(ns + "OSImage",
+                             new XElement(ns + "Label", Properties.Label),
+                             new XElement(ns + "MediaLink", Properties.MediaLink),
+                             new XElement(ns + "Name", Properties.Name),
+                             new XElement(ns + "OS", Properties.OperatingSystem.ToString()),
+                             new XElement(ns + "PublishedDate", Properties.PublishedDate.ToString(CultureInfo.InvariantCulture)),
+                             new XElement(ns + "IsPremium", Properties.IsPremium.ToString().ToLowerInvariant()),
+                             new XElement(ns + "ShowInGui", Properties.ShowInGui.ToString().ToLowerInvariant())));
+            return doc.ToStringFullXmlDeclaration();
+        }
+
+        /// <summary>
+        /// returns the name of the command
+        /// </summary>
+        public override string ToString()
+        {
+            return "RegisterImageCommand";
+        }
+    }
+}
---
+++ Elastacloud.AzureManagement.Fluent/Types/Virtual Machines/ImageProperties.cs
@@ -0,0 +1,31 @@
+﻿/************************************************************************************************************
+ * This software is distributed under a GNU Lesser License by Elastacloud Limited and it is free to         *
+ * modify and distribute providing the terms of the license are followed. From the root of the source the   *
+ * license can be found in /Resources/license.txt                                                           * 
+ *                                                                                                          *
+ * Web at: www.elastacloud.com                                                                              *
+ * Email: info@elastacloud.com                                                                              *
+ ************************************************************************************************************/
+
+
+using System;
+
+namespace Elastacloud.AzureManagement.Fluent.Types.VirtualMachines
+{
+    /// <summary>
+    /// Contains the detailed information for image property generation 
+    /// </summary>
+    public class ImageProperties
+    {
+        public string Label { get; set; }
+        public string Name { get; set; }
+        public string MediaLink { get; set; }
+        public PlatformType OperatingSystem { get; set; }
+        public string Eula { get; set; }
+        public string Description { get; set; }
+        public string ImageFamily { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public bool? IsPremium { get; set; }
+        public bool? ShowInGui { get; set; }
+    }
+}
