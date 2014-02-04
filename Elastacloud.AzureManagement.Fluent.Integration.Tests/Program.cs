099951857507b66061cca6309a2b863d400ec8dc
Richard Conway <richard@elastacloud.com>
2014-02-04 13:01:28 +0000

    updated register imagecommand and workflow

---
+++ Elastacloud.AzureManagement.Fluent.Integration.Tests/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elastacloud.AzureManagement.Fluent.Clients;
+using Elastacloud.AzureManagement.Fluent.Helpers.PublishSettings;
+using Elastacloud.AzureManagement.Fluent.Types.VirtualMachines;
+
+namespace Elastacloud.AzureManagement.Fluent.Integration.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // should have 5 arguments though subscription_id_source, path_to_pub_settings_source,
+            // destination_account_name, destination_account_key, vhd_blob_path
+            // 1. test the copy of the image from source to destination account
+            // ReSharper disable once InconsistentNaming
+            string subscription_id_source = args[0];
+            // ReSharper disable once InconsistentNaming
+            var certificate_source = new PublishSettingsExtractor(args[1]).AddPublishSettingsToPersonalMachineStore();
+            // ReSharper disable once InconsistentNaming
+            string account_name_destination = args[2];
+            // ReSharper disable once InconsistentNaming
+            string account_key_destination = args[3];
+            // ReSharper disable once InconsistentNaming
+            string source_image_path = args[4];
+            var properties = new ImageProperties()
+            {
+                OperatingSystem = PlatformType.Linux,
+                Description = "Test from Azure Fluent Management",
+                ShowInGui = false,
+                IsPremium = true
+            };
+            var client = new ImageManagementClient(subscription_id_source, certificate_source);
+            client.CopyAndRegisterImageInNewSubscription(account_name_destination, account_key_destination, 
+                "elastaimage", "sparkius", source_image_path, properties);
+        }
+    }
+}
